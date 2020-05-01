2d93c7912b735688e8a213be69216499c335577a
Tincho06 <gp2hdis@gmail.com>
2020-05-01 17:34:05 -0300

    Create ejercicio01.js

---
+++ ejercicio01.js
@@ -0,0 +1,8 @@
+let miComidaPreferida = {
+  nombre: 'Empanadas',
+  cantidadDeVecesQueComi: 195,
+  cantidadDeVecesQueMeCocinaron: 100,
+  ingredientes: ['carne', 'cebolla', 'verdeo', 'huevo duro', 'comino', 'pimentón', 'aji', 'sal']
+}
+
+miComidaPreferida.push()
