2749bfd61a2e20e42fd3bd2626a65b09394a58b1
Tincho06 <gp2hdis@gmail.com>
2020-05-04 17:10:01 -0300

    Update ej_investigar.js

--- ej_investigar.js
+++ ej_investigar.js
@@ -36,3 +36,51 @@ function obtenerRecetaPorIngrediente(listaComida, ingrediente) {
 console.log(obtenerRecetaPorIngrediente(comidas, 'Sal'))
 console.log(obtenerRecetaPorIngrediente(comidas, 'Comino'))
 console.log(obtenerRecetaPorIngrediente(comidas, 'Ajo'))
+
+
+/*Definir una función llamada obtenerRecetaPorIngredientes que funcione igual, pero el
+segundo parámetro, en vez de ser el nombre de un ingrediente, que sea una lista de
+ingredientes. Debe devolver todas las recetas que puedan hacerse con esos
+ingredientes.*/
+
+let comidas = [
+  {
+    nombre: 'Empanadas',
+    ingredientes: ['Carne', 'Cebolla', 'Verdeo', 'Ajo', 'Sal', 'Comino']
+  },
+  {
+    nombre: 'Canelones',
+    ingredientes: ['Espinaca', 'Ricota', 'Nuez', 'Tomate', 'Sal', 'Queso']
+  },
+  {
+    nombre: 'Asado',
+    ingredientes: ['Carne','Sal']
+  },
+  {
+    nombre: 'Milanesas',
+    ingredientes: ['Carne', 'Huevo', 'Pan rallado', 'Ajo', 'Sal']
+  },
+]
+
+let ingredientesLista = ['carne', 'Ajo']
+
+
+function obtenerRecetaPorIngredientes(listaComida, listaIngredientes) {
+  comidasEncontradas = []
+  ingredientes = []
+
+  for (i = 0; i < ingredientesLista.length; i++) {
+    ingredientes.push(ingredientesLista[i])
+  }
+
+  for (i = 0; i < comidas.Object.ingredientes.length; i++) {
+    comidasEncontradas.push(comidas[i])
+  }
+
+  if (comidasEncontradas.includes(ingredientes[i])) {
+
+  return comidasEncontradas
+  }
+}
+
+console.log(obtenerRecetaPorIngredientes(comidas, ingredientesLista))
