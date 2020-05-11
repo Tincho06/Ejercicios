d9fedd19319ca27c8ee12c392925954c4ae1ccbf
Tincho06 <gp2hdis@gmail.com>
2020-05-11 14:51:14 -0300

    Tp estudio

---
+++ TP_EstudioGrab/main.js
@@ -0,0 +1,50 @@
+//Clientes
+
+listaClientes = [
+  {
+    nombreBanda: 'aaaa',
+    telefono: 123456,
+    sesiones: [
+      1 = {
+        fecha: 01/01/2020,
+        ingreso: 15:50,
+        salida: 17:30,
+        duracion: salida - ingreso,
+      }
+    ]
+    pagos: [
+      {
+        fecha: 01/01/2020,
+        monto: 150,
+      }
+
+    ]
+  },
+  {
+    nombreBanda: 'bbbb',
+    telefono: 123456,
+    sesiones: [
+      1 = {
+        fecha: 05/01/2020,
+        ingreso: 18:30,
+        salida: 19:30,
+        duracion: salida - ingreso,
+      }
+    ]
+    pagos: [
+      {
+        fecha: 06/01/2020,
+        monto: 100,
+      }
+
+    ]
+  }
+]
+
+
+function cargaCliente() {
+  const cliente = prompt('Nombre de Banda', '')
+  if (cliente != null) {
+    document.querySelector('.confirma').innerHTML = 'La banda ' + cliente + 'fue cargada'
+  }
+}
--- ej_investigar2.js
+++ ej_investigar2.js
@@ -1,1 +1,4 @@
-ej_investigar
+/*1. Definir calcularVuelto que reciba un precio a pagar y un monto con el que se paga y debe
+expresar el vuelto en billetes (cuántos y cuáles), buscando que se entregue la menor
+cantidad de billetes posible.*/
+
