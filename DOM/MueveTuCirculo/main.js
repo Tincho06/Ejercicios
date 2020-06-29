33d503b5398877429314eeb93fe685d2645beafd
Tincho06 <gp2hdis@gmail.com>
2020-06-29 13:37:42 -0300

    Bonus point 1
    Cuando el círculo llega al lìmite de la pantalla desaparece y reaparece del lado opuesto de la pantalla.

--- DOM/MueveTuCirculo/main.js
+++ DOM/MueveTuCirculo/main.js
@@ -1,8 +1,31 @@
+/*
+Crear una aplicación que tenga un círculo inicialmente rojo, un tablero de control con
+cuatro flechas (arriba, izquierda, derecha y abajo) y una configuración de colores.
+
+Mediante el tablero de control el círculo debe poder moverse hacia el lado que fue
+indicado. Lo mismo debe suceder con las teclas: si aprieto la tecla derecha, el círculo debe
+moverse hacia la derecha, si aprieto la izquierda, hacia la izquierda.
+
+El color del círculo debe poder elegirse entre rojo, azul y verde.
+
+Bonus points:
+• ¿Qué pasa si el círculo llega a un extremo de la pantalla? Pensar cómo solucionar este
+problema (si es que lo es) y programar la solución.
+   - Cuando el cìrculo llega al lìmite de la pantalla debe desaparecer y reaparecer del lado opuesto.
+
+• Agregar una opción para que el círculo se mueva solo aleatoriamente.
+
+• Agregar otra opción para que el círculo se mueva diagonalmente y cambiar su rumbo
+al chocarse con una pared.
+ */
+
 window.onload = function() {
   
   const container = document.querySelector('.container')
   const containerHeight = document.querySelector('.container').offsetHeight
   const containerWidth = document.querySelector('.container').offsetWidth
+  //console.log(containerHeight)
+  //console.log(containerWidth)
 
   const arrows = document.querySelectorAll('.arrow')
 
@@ -12,6 +35,8 @@ window.onload = function() {
   const down = document.querySelector('.down')
 
   const circle = document.querySelector('.circle')
+  const circleHeight = document.querySelector('.circle').offsetHeight
+  const circleWidth = document.querySelector('.circle').offsetWidth
 
   const red = document.querySelector('.red')
   const blue = document.querySelector('.blue')
@@ -23,29 +48,49 @@ window.onload = function() {
   function moveCircle(object) {
     const key = object.which
 
-    const circleY = document.querySelector('.circle').offsetLeft
-    const circleX = document.querySelector('.circle').offsetTop
+    const circleX = document.querySelector('.circle').offsetLeft
+    const circleY = document.querySelector('.circle').offsetTop
 
-    //Secuencia de sentencias condicionales
-        switch (key){
-            case 37 :   
-                circle.style.left = circleY-10+"px" 
-                circle.classList.add('animacion')  ; break
-                
-            case 38 :
-                circle.style.top = circleX-10+"px"
-                circle.classList.add('animacion')  ; break
-                
-            case 39 : 
-                circle.style.left = circleY+10+"px"
-                circle.classList.add('animacion')  ; break
-                
-            case 40 :
-                circle.style.top = circleX+10+"px"
-                circle.classList.add('animacion')  ; break
-                
-        //default :alert("Solo se mueve con las flechas!")
-        }
+    if (key === 37) {
+      
+      if (circleX < containerWidth && circleX > 0) { 
+        circle.style.left = circleX-10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.left = containerWidth - circleWidth+"px"
+      }
+    }
+
+    if (key === 38) {
+      
+      if (circleY < containerHeight && circleY > 0) { 
+        circle.style.top = circleY-10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.top = containerHeight - circleHeight+"px"
+      }
+    }
+
+    if (key === 39) {
+      
+      if (circleX <= containerWidth - circleWidth) { 
+        circle.style.left = circleX+10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.left = 0 +"px" //containerWidth - circleX+"px"
+      }
+    }
+
+    if (key === 40) {
+      
+      if (circleY <= containerHeight - circleHeight) { 
+        circle.style.top = circleY+10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.top = 0 +"px" //containerHeight - circleY+"px"
+      }
+    }
+
   }
 
   //Mover con las flechas
@@ -54,37 +99,59 @@ window.onload = function() {
   //Mover con botón de pantalla UP
   up.onclick = function() {
 
-    const circleX = document.querySelector('.circle').offsetTop
+    const circleY = document.querySelector('.circle').offsetTop
     
-    circle.style.top = circleX-10+"px"
-    circle.classList.add('animacion')
+    if (circleY < containerHeight && circleY > 0) { 
+      circle.style.top = circleY-10+"px" 
+      circle.classList.add('animacion')
+    } else {
+      circle.style.top = containerHeight - circleHeight+"px"
+    }
+    
 
   }
 
   //Mover con botón de pantalla DOWN
   down.onclick = function() {
 
-    const circleX = document.querySelector('.circle').offsetTop
+    const circleY = document.querySelector('.circle').offsetTop
+      
+    if (circleY < containerHeight - circleHeight) { 
+        circle.style.top = circleY+10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.top = 0 +"px" //containerHeight - circleY+"px"
+      }
     
-    circle.style.top = circleX+10+"px"
     
   }
 
   //Mover con botón de pantalla LEFT
   left.onclick = function() {
 
-    const circleY = document.querySelector('.circle').offsetLeft
+    const circleX = document.querySelector('.circle').offsetLeft
+      
+    if (circleX < containerWidth && circleX > 0) { 
+      circle.style.left = circleX-10+"px" 
+      circle.classList.add('animacion')
+    } else {
+      circle.style.left = containerWidth - circleWidth+"px"
+    }
     
-    circle.style.left = circleY-10+"px"
     
   }
 
   //Mover con botón de pantalla RIGTH
   right.onclick = function() {
 
-    const circleY = document.querySelector('.circle').offsetLeft
+    const circleX = document.querySelector('.circle').offsetLeft
     
-    circle.style.left = circleY+10+"px"
+    if (circleX < containerWidth - circleWidth) { 
+        circle.style.left = circleX+10+"px" 
+        circle.classList.add('animacion')
+      } else {
+        circle.style.left = 0 +"px" //containerWidth - circleX+"px"
+      }
     
   }
 
@@ -142,7 +209,7 @@ window.onload = function() {
     }
   })
 
-  // controlY, determina dirección vertical: 1-baja|0-sube
+  /*// controlY, determina dirección vertical: 1-baja|0-sube
   let controlY = Math.floor((Math.random() * 1) + 0)
 
 
@@ -162,7 +229,7 @@ window.onload = function() {
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
-  getRandomInt(5,20)
+  getRandomInt(5,20)*/
 
 }
 
