6147b139bafb22c4af8b34afff55419199c97122
Tincho06 <gp2hdis@gmail.com>
2020-07-08 14:03:45 -0300

    Agrego random
    Agrego función de activar y desactivar random y cambio la propiedad de css para el movimiento de margen izquierdo y margen superior  a posición absoluta left - top

--- DOM/MueveTuCirculo_2/main.js
+++ DOM/MueveTuCirculo_2/main.js
@@ -1,4 +1,4 @@
-const MOVEMENT = 50
+const MOVEMENT = 60
 
 let horizontalPosition = 0
 let verticalPosition = 0
@@ -17,8 +17,8 @@ window.onload = function() {
   const circle = document.querySelector('.circle')
   const circleHeight = document.querySelector('.circle').offsetHeight
   const circleWidth = document.querySelector('.circle').offsetWidth
-  const circleX = document.querySelector('.circle').offsetLeft
-  const circleY = document.querySelector('.circle').offsetTop
+  //const circleX = document.querySelector('.circle').offsetLeft
+  //const circleY = document.querySelector('.circle').offsetTop
 
   const red = document.querySelector('.red')
   const blue = document.querySelector('.blue')
@@ -34,31 +34,39 @@ window.onload = function() {
   
 
   function moveCircleToLeft() {
-    if (circle.offsetLeft > +10) {
+    const circleY = document.querySelector('.circle').offsetLeft
+    circle.style.left = circleY - MOVEMENT
+    /*if (circle.offsetLeft > +10) {
     horizontalPosition -= MOVEMENT
       circle.style.marginLeft = horizontalPosition
-    }
+    }*/
   }
 
   function moveCircleToRight() {
-    if (circle.offsetLeft < rightLimit) {
+    const circleY = document.querySelector('.circle').offsetLeft
+      circle.style.left = circleY + MOVEMENT
+    /*if (circle.offsetLeft < rightLimit) {
     horizontalPosition += MOVEMENT
     circle.style.marginLeft = horizontalPosition
-    }
+    }*/
   }
 
   function moveCircleToTop() {
-    if (circle.offsetTop > +10) {
+    const circleX = document.querySelector('.circle').offsetTop
+    circle.style.top = circleX - MOVEMENT
+    /*if (circle.offsetTop > +10) {
     verticalPosition -= MOVEMENT
     circle.style.marginTop = verticalPosition
-    }
+    }*/
   }
 
   function moveCircleTobottom() {
-    if (circle.offsetTop < bottomLimit) {
+    const circleX = document.querySelector('.circle').offsetTop
+    circle.style.top = circleX + MOVEMENT
+    /*if (circle.offsetTop < bottomLimit) {
     verticalPosition += MOVEMENT
     circle.style.marginTop = verticalPosition
-    }
+    }*/
   }
 
   left.onclick = moveCircleToLeft
@@ -73,21 +81,46 @@ window.onload = function() {
     switch (event.keyCode) {
       case 37:
         moveCircleToLeft()
+        left.style = 'opacity: 0.7'
         return
 
       case 38:
         moveCircleToTop()
+        up.style = 'opacity: 0.7'
         return
 
       case 39:
         moveCircleToRight()
+        right.style = 'opacity: 0.7'
         return
 
       case 40:
         moveCircleTobottom()
+        down.style = 'opacity: 0.7'
         return
 
     }
+  }
+
+    document.onkeyup = function (event) {
+      switch (event.keyCode) {
+      case 37:
+        left.style = 'opacity: 1'
+        return
+
+      case 38:
+        up.style = 'opacity: 1'
+        return
+
+      case 39:
+        right.style = 'opacity: 1'
+        return
+
+      case 40:
+        down.style = 'opacity: 1'
+        return
+    }
+
   }
 
   red.onclick = function() {
@@ -102,7 +135,43 @@ window.onload = function() {
     circle.style.background = 'green'
   }
 
+  function randomMove() {
+    const movimientos = [
+      'arriba',
+      'abajo',
+      'izquierda',
+      'derecha',
+    ]
+
+    const randomNumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0
+    console.log(randomNumber)
+
+    const movimiento = movimientos[randomNumber]
+
+    if (movimiento === 'arriba') {
+      moveCircleToTop()
+    } else if (movimiento === 'abajo') {
+      moveCircleTobottom()
+    } else if (movimiento === 'izquierda') {
+      moveCircleToLeft()
+    } else if (movimiento === 'derecha') {
+      moveCircleToRight()
+    }
+
+  }
+
   
 
+  play.onclick = function() {
+
+    setInterval(randomMove, 200)
+  }
+
+  pause.onclick = function() {
+
+    location.reload()
+  }
+
+  
 
 }
